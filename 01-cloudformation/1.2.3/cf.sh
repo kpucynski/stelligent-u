ca7a3a20abb66bfc0ee0484a8a24d8cea1a9c5d2
Karol Pucynski <karol@pucynski.pl>
2021-10-05 14:53:46 +0200

    1.2.3

---
+++ 01-cloudformation/1.2.3/cf.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+REGION="${2:-us-east-2}"
+
+aws cloudformation $1-stack --stack-name kpu-lab-02-1 --template-body file://iam.yaml --parameters file://iam.json --region ${REGION} --capabilities CAPABILITY_NAMED_IAM
